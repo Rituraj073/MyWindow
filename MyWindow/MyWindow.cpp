6fa3b866df66867cea159004ef006cf30db591e3
Rituraj Prasad <rituraj.prasad2718@gmail.com>
2026-04-20 17:42:27 +0530

    Make button

--- MyWindow/MyWindow.cpp
+++ MyWindow/MyWindow.cpp
@@ -1,21 +1,48 @@
 #include <windows.h>
 
+#define BUTTON_ID 1
+
 // Window Procedure
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    static HWND hButton;
+
     switch (uMsg) {
 
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        // Draw text
-        const wchar_t* text = L"Hello Rituraj";
-        TextOut(hdc, 100, 100, text, lstrlen(text));
-
-        EndPaint(hwnd, &ps);
+    case WM_CREATE:
+        // Create a button
+        hButton = CreateWindow(
+            L"BUTTON",                    // Predefined class
+            L"Click Me",                  // Button text
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            100, 150, 120, 40,            // Position and size
+            hwnd,                         // Parent window
+            (HMENU)BUTTON_ID,             // Button ID
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+            NULL
+        );
         return 0;
-    }
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == BUTTON_ID) {
+            MessageBox(hwnd, L"Hello Rituraj!", L"Info", MB_OK);
+        }
+        return 0;
+
+    //case WM_PAINT:
+    //{
+    //    PAINTSTRUCT ps;
+    //    HDC hdc = BeginPaint(hwnd, &ps);
+
+    //    // Fill background
+    //    FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+    //    // Draw text
+    //    const wchar_t* text = L"Hello Rituraj";
+    //    TextOut(hdc, 100, 100, text, lstrlen(text));
+
+    //    EndPaint(hwnd, &ps);
+    //    return 0;
+    //}
 
     case WM_DESTROY:
         PostQuitMessage(0);
@@ -27,23 +54,17 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
 // Entry Point
 int main() {
-    // Get instance handle
     HINSTANCE hInstance = GetModuleHandle(NULL);
-
     const wchar_t CLASS_NAME[] = L"MyWindowClass";
 
-    // Register Window Class
     WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // Auto background
 
-    if (!RegisterClass(&wc)) {
-        return 0;
-    }
+    RegisterClass(&wc);
 
-    // Create Window
     HWND hwnd = CreateWindowEx(
         0,
         CLASS_NAME,
@@ -56,16 +77,11 @@ int main() {
         NULL
     );
 
-    // Check if creation failed
-    if (hwnd == NULL) {
-        return 0;
-    }
+    if (hwnd == NULL) return 0;
 
-    // Show Window
     ShowWindow(hwnd, SW_SHOW);
     UpdateWindow(hwnd);
 
-    // Message Loop
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
